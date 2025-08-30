1dab927539fec5d31d9aec1aca385b869797fb78
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-08-30 23:49:06 +0530

    new approach added

--- BinarySearch/sum-of-square-numbers.cpp
+++ BinarySearch/sum-of-square-numbers.cpp
@@ -42,3 +42,25 @@ public:
 
     }
 };
+
+
+much better Solution
+
+class Solution {
+public:
+    
+     
+bool judgeSquareSum(int c) {
+        
+    long long a = 0;
+    long long b = sqrt(c);
+    while (a <= b) {
+        long long sum = a*a + b*b;
+        if (sum == c) return true;
+        else if (sum < c) a++;
+        else b--;
+    }
+    return false;
+
+    }
+};
